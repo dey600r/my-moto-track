895fde0928ef548b4a2daecc466709463c469dbf
dey600r <dey125r@gmail.com>
2019-07-30 19:23:45 +0200

    Add packaged grid generic publish

---
+++ public_api.ts
@@ -0,0 +1,4 @@
+export * from './src/app/shared/module/shared.module';
+export * from './src/app/shared/model/motos.model';
+export * from './src/app/shared/service/motos.service';
+export * from './src/app/shared/model/common.model';
